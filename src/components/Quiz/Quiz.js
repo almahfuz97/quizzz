9cf32fb4128ad09041702296ae9f8012726c7b15
almahfuz <almahfuz0327@gmail.com>
2022-10-11 03:30:47 +0600

    added toastify on click

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import QuizLayout from "../QuizLayout/QuizLayout";
 import Topic from "../Topic/Topic";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Quiz() {
   const quizes = useLoaderData();
@@ -15,16 +17,25 @@ export default function Quiz() {
       const newAns = { ...answer };
       newAns.corrAns++;
       setAnswer(newAns);
+      showToast(0);
     } else {
       const newAns = { ...answer };
       newAns.wrongAns++;
       setAnswer(newAns);
+      showToast(1);
     }
+  };
+
+  const showToast = (x) => {
+    x === 0
+      ? toast("Correct Answer", { position: toast.POSITION.TOP_CENTER })
+      : toast("Wrong Answer", { position: toast.POSITION.TOP_CENTER });
   };
 
   console.log(quizes);
   return (
     <div>
+      <ToastContainer autoClose={200} />
       <div className="my-10 flex justify-between items-center">
         <h1 className="text-center font-bold text-2xl text-red-500">
           {quizes.data.name}
